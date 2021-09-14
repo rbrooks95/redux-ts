37f4dc83dc3b7a2c9099cd034c90190087280e4b
Roshawn Brooks <69084474+rbrooks95@users.noreply.github.com>
2021-09-13 23:53:18 -0400

    Second  commit

---
+++ src/state/actions/index.ts
@@ -0,0 +1,18 @@
+import { ActionType } from "../action-type";
+
+interface SearchRepositoriesAction {
+  type: ActionType.SEARCH_REPOSOITORIES;
+  payload: string[];
+}
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+interface SearchRepositioriesErorAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+export type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesSuccessAction
+  | SearchRepositioriesErorAction;
